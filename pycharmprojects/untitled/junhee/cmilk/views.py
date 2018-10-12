eeca278ff03ffcc1846c287e6e92205c1ee5d117
cmilk18 <43445697+cmilk18@users.noreply.github.com>
2018-10-12 17:45:28 +0900

    Update views.py

--- pycharmprojects/untitled/junhee/cmilk/views.py
+++ pycharmprojects/untitled/junhee/cmilk/views.py
@@ -2,33 +2,48 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 import json
-
-
-
-
+ 
+ 
+ 
 def keyboard(request):
-
+ 
     return JsonResponse({
-        "type": "buttons", "text"
-        "buttons": ["강아지 사료양"]
+        'type':'buttons',
+        'buttons':['오늘','내일']
     })
-
+ 
 @csrf_exempt
 def answer(request):
-
+ 
     json_str = ((request.body).decode('utf-8'))
     received_json_data = json.loads(json_str)
     datacontent = received_json_data['content']
-
-    if datacontent == "강아지 사료양":
-        global section
-        section = '사료1'
-
+ 
+    if datacontent == '오늘':
+        today = "오늘 급식"
+ 
         return JsonResponse({
-            'message': {
-                'text': "강아지의 몸무게(kg)를 알려주세요!"
-            },
-            'keyboard': {
-                'type': 'text'
-            }
-        })
+                'message': {
+                    'text': today
+                },
+                'keyboard': {
+                    'type':'buttons',
+                    'buttons':['오늘','내일']
+                }
+ 
+            })
+ 
+    elif datacontent == '내일':
+        tomorrow = "내일 급식"
+ 
+        return JsonResponse({
+                'message': {
+                    'text': tomorrow
+                },
+                'keyboard': {
+                    'type':'buttons',
+                    'buttons':['오늘','내일']
+                }
+ 
+            })
+ 
