75ce4bbff83e70b71c6af7e378b3724437ef24f1
cmilk18 <43445697+cmilk18@users.noreply.github.com>
2018-11-16 23:27:19 +0900

    Update manage.py

--- pycharmprojects/untitled/junhee/manage.py
+++ pycharmprojects/untitled/junhee/manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stu.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'junhee.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
